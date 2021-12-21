5929de6e4eabb86e04c06cd1cfe0177705385898
Trần Bảo Việt <AT150165@actvn.edu.vn>
2021-12-21 15:47:32 +0700

    Add project files.

---
+++ StudentManagerWebApp/Models/SchoolContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentManagerWebApp.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(@"Server=.\BAOVIET;Database=SchoolDB;Trusted_Connection=True;");
+        //}
+    }
+}
