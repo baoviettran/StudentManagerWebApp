3befb8397f0f8e4572c7ea7e5e45ddd73562ff5c
Trần Bảo Việt <AT150165@actvn.edu.vn>
2021-12-23 17:36:35 +0700

    create StudentManagement_WebAPI

---
+++ StudentManagement_WebAPI/Models/SchoolDBContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace StudentManagement_WebAPI.Models
+{
+    public partial class SchoolDBContext : DbContext
+    {
+        //public SchoolDBContext()
+        //{
+        //}
+
+        public SchoolDBContext(DbContextOptions<SchoolDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<UserInfo> UserInfos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=LEILA\\BAOVIET;Database=SchoolDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Vietnamese_CI_AS");
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.ToTable("Student");
+            });
+
+            modelBuilder.Entity<UserInfo>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("UserInfo");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ StudentManagement_WebAPI/Models/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace StudentManagement_WebAPI.Models
+{
+    public partial class UserInfo
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
