23fc0f7a315137bc32453e82d1f841e608dcd788
Trần Bảo Việt <AT150165@actvn.edu.vn>
2021-12-24 13:03:20 +0700

    Clean & Rename

---
+++ UserManagement_WebAPI/Models/SchoolDBContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace UserManagement_WebAPI.Models
+{
+    public partial class SchoolDBContext : DbContext
+    {
+        //public SchoolDBContext()
+        //{
+        //}
+
+        public SchoolDBContext(DbContextOptions<SchoolDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Vietnamese_CI_AS");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("User");
+
+                entity.Property(e => e.UserId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("UserID");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ UserManagement_WebAPI/Models/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace UserManagement_WebAPI.Models
+{
+    public partial class UserDTO
+    {
+        
+        public string Email { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
